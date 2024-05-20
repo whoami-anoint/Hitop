1ab77c8177171edd8a772c1df7b8c249be9d04ea
Anoint <72187543+whoami-anoint@users.noreply.github.com>
2024-05-20 15:49:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,14 +15,20 @@ def get_current_time():
     return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 def get_running_services():
-    # Run the 'systemctl' command to list running services
-    services_output = subprocess.check_output(['systemctl', '--no-pager', 'list-units', '--state', 'running', '--plain'])
-    return services_output.decode('utf-8')
+    try:
+        # Run the 'systemctl' command to list running services
+        services_output = subprocess.check_output(['systemctl', '--no-pager', 'list-units', '--state', 'running', '--plain'])
+        return services_output.decode('utf-8')
+    except FileNotFoundError:
+        return "systemctl not found"
 
 def get_inactive_services():
-    # Run the 'systemctl' command to list inactive services
-    inactive_services_output = subprocess.check_output(['systemctl', '--no-pager', 'list-units', '--state', 'inactive', '--plain'])
-    return inactive_services_output.decode('utf-8')
+    try:
+        # Run the 'systemctl' command to list inactive services
+        inactive_services_output = subprocess.check_output(['systemctl', '--no-pager', 'list-units', '--state', 'inactive', '--plain'])
+        return inactive_services_output.decode('utf-8')
+    except FileNotFoundError:
+        return "systemctl not found"
 
 @app.route('/')
 def index():
@@ -35,8 +41,9 @@ def index():
     # Split the top output into lines
     top_lines = top_output.split('\n')
 
-    running_services_list = [line.split() for line in running_services.split('\n') if line]
-    inactive_services_list = [line.split() for line in inactive_services.split('\n') if line]
+    # Split the running and inactive services into lists
+    running_services_list = [line.split() for line in running_services.split('\n') if line != "systemctl not found"]
+    inactive_services_list = [line.split() for line in inactive_services.split('\n') if line != "systemctl not found"]
 
     # Pass the information to the template
     return render_template('index.html', top_lines=top_lines, current_time=current_time,
@@ -47,10 +54,13 @@ def running_services():
     # Get running services
     running_services = get_running_services()
 
-    running_services_list = [line.split() for line in running_services.split('\n') if line]
+    # Split the running services into a list
+    running_services_list = [line.split() for line in running_services.split('\n') if line != "systemctl not found"]
 
+    # Pass the information to the template
     return render_template('running_services.html', running_services=running_services_list)
 
 if __name__ == '__main__':
+    # Get the port from environment variable PORT or default to 5000
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
